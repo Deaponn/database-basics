10e8000c276fd7b67df758f7223019b94b2f1ad8
Deaponn <bartek.sajo@interia.pl>
2023-11-14 13:16:15 +0100

    Correct solution to Lab4 exercise

--- lab4/home_exercises.sql
+++ lab4/home_exercises.sql
@@ -1,12 +1,13 @@
 -- zad 4
 -- a)
-SELECT EmpA.FirstName, EmpA.LastName, EmpA.EmployeeID, 
+SELECT DISTINCT EmpA.FirstName, EmpA.LastName, EmpA.EmployeeID, 
 ROUND(SUM(ROUND(Quantity * UnitPrice * (1 - Discount), 2)), 2) AS [wartosc zamowien]
 FROM Employees AS EmpA
 INNER JOIN Employees AS EmpB ON EmpA.EmployeeID = EmpB.ReportsTo
 INNER JOIN Orders ON EmpA.EmployeeID = Orders.EmployeeID
 INNER JOIN [Order Details] ON Orders.OrderID = [Order Details].OrderID
-GROUP BY EmpA.EmployeeID, EmpA.FirstName, EmpA.LastName
+-- GROUP BY EmpA.EmployeeID, EmpA.FirstName, EmpA.LastName
+GROUP BY EmpB.EmployeeID, EmpA.EmployeeID, EmpA.FirstName, EmpA.LastName
 ORDER BY 3
 
 -- b)
@@ -19,4 +20,3 @@ INNER JOIN [Order Details] ON Orders.OrderID = [Order Details].OrderID
 WHERE EmpB.EmployeeID IS NULL
 GROUP BY EmpA.EmployeeID, EmpA.FirstName, EmpA.LastName
 ORDER BY 3
-
